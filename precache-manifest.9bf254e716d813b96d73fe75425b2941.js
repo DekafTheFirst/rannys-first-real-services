a0479ea35d09b4cd250e5f8843ff7efc69b62689
Destiny Kefas <dekeji1@gmail.com>
2022-05-30 22:19:18 +0800

    Updates

---
+++ precache-manifest.9bf254e716d813b96d73fe75425b2941.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6e1eda6874c9adf4cdd",
+    "url": "/rannys-first-real-services/static/css/main.784db991.chunk.css"
+  },
+  {
+    "revision": "e6e1eda6874c9adf4cdd",
+    "url": "/rannys-first-real-services/static/js/main.e6e1eda6.chunk.js"
+  },
+  {
+    "revision": "7d1c24b692b972ccd288",
+    "url": "/rannys-first-real-services/static/css/1.fa13fcda.chunk.css"
+  },
+  {
+    "revision": "7d1c24b692b972ccd288",
+    "url": "/rannys-first-real-services/static/js/1.7d1c24b6.chunk.js"
+  },
+  {
+    "revision": "305e3d6dde58deb8eb89",
+    "url": "/rannys-first-real-services/static/js/runtime~main.305e3d6d.js"
+  },
+  {
+    "revision": "06b3918dd79c9ce356f0c9bae9fb1e3b",
+    "url": "/rannys-first-real-services/index.html"
+  }
+];
